0191a482d73945d70291c3d0b68c4435ddb723ba
고경우 <dfdfg11@gmail.com>
2023-08-12 22:44:26 +0900

    [level 2] Title: 오픈채팅방, Time: 171.89 ms, Memory: 39.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    
+    map<string,string> m;
+    vector<vector<string>> seq;
+    
+    //seq 실행 목록 저장
+
+
+        for(auto r: record){
+            stringstream ss;
+            ss.str(r);
+            string enter,id,name;
+            ss >> enter >> id >> name;
+            
+            vector<string> sol;
+            sol.push_back(enter);
+            sol.push_back(id);
+            sol.push_back(name);
+            seq.push_back(sol);
+            
+            if(enter == "Enter" || enter == "Change"){
+                m[id] = name;
+            }
+
+            sol.clear();
+        }
+    //출력
+    for(int i=0; i<seq.size(); i++){
+        if(seq[i][0] == "Enter"){
+            answer.push_back(m[seq[i][1]] + "님이 들어왔습니다.");
+
+        }
+        else if(seq[i][0] == "Leave"){
+            answer.push_back(m[seq[i][1]] + "님이 나갔습니다.");
+
+        }
+    }
+
+    
+    return answer;
+}
