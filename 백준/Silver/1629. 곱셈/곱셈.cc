d97f4aad05dfe3344934c3aab0b836ff60914f12
고경우 <dfdfg11@gmail.com>
2023-08-14 15:44:00 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,39 +1,28 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_map>
-typedef long long ll;
 
+typedef long long ll;
 using namespace std;
 
+ll A, B, C;
 
-
-
-ll result, A,B,C;
 ll calc(ll a, ll b) {
-
     if (b == 0) return 1;
-    if (b == 1) return a;
 
+    ll result = calc(a, b >> 1);
+    result = (result * result) % C;
 
+    if (b & 1) result = (result * a) % C;
 
-    ll result = (calc(a, b / 2) * calc(a, b / 2))%C;
-    
-    if (b % 2 == 1) (result = result * a)%C;
-    
-
-    return (result%C);
+    return result % C;
 }
 
 int main() {
-
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     cin >> A >> B >> C;
-
-
     cout << calc(A, B) % C;
-    
+
+    return 0;
 }
