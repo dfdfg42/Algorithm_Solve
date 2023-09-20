446d4c7573da067ccd2b1be23018f2459053aa69
고경우 <dfdfg11@gmail.com>
2023-09-20 22:09:16 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[9];
+bool visited[9];
+int save[9];
+int n,m;
+
+
+void go(int at, int depth) {
+    if (depth == m) {
+         
+        for (int i = 0; i < m; i++) {
+            cout << save[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    int last = 0;
+    for (int i = at; i < n; i++) {
+        if (arr[i] != last) {
+            save[depth] = arr[i];
+            last = arr[i];
+
+            go(i, depth + 1);
+        }
+        
+        
+       
+    }
+}
+
+int main() {
+
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    
+    go(0, 0);
+
+    return 0;
+}
