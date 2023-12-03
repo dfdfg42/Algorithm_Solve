e9bf415ebb749d6434cdc4a350b396fea774759f
고경우 <dfdfg11@gmail.com>
2023-12-03 19:11:12 +0900

    [Silver I] Title: Fraction, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/30855. Fraction/Fraction.cc
+++ 백준/Silver/30855. Fraction/Fraction.cc
@@ -1,78 +1,67 @@
 #include <iostream>
 #include <stack>
 #include <utility>
-#include <vector>
-#include <sstream>
+#include <string>
 
 using namespace std;
 
 typedef pair<long long, long long> frac;
 
+
 long long gcd(long long a, long long b) {
-    if (b == 0) return a;
-    return gcd(b, a % b);
+    while (b != 0) {
+        long long t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
 }
 
-frac add(frac x, frac y) {
-    long long pa = x.first * y.second + x.second * y.first;
-    long long pb = x.second * y.second;
-    long long k = gcd(pa, pb);
-    return make_pair(pa / k, pb / k);
-}
-
-frac gl(frac x, frac y) {
-    long long pa = x.first * y.second;
-    long long pb = x.second * y.first;
-    long long k = gcd(pa, pb);
-    return make_pair(pa / k, pb / k);
-}
-
-frac sv(frac x, frac y, frac z) {
-    return add(x, gl(y, z));
-}
 
 int main() {
     int n;
     cin >> n;
-    cin.ignore();
-
-    vector<string> a(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
 
     stack<frac> s;
 
-    for (auto& i : a) {
-        if (i == ")") {
-            if (s.size() < 4) {
-                cout << -1 << endl;
-                return 0;
-            }
+    for (int i = 0; i < n; ++i) {
+        string token;
+        cin >> token;
 
+        if (token == "(") {
+            s.push(make_pair(-1, -1)); // '('를 표시하기 위한 더미 값
+        }
+        else if (token == ")") {
             frac z = s.top(); s.pop();
             frac y = s.top(); s.pop();
             frac x = s.top(); s.pop();
+            s.pop(); // '(' 더미 값 제거
 
-            if (s.top().first != -1) {
-                cout << -1 << endl;
-                return 0;
-            }
-            s.pop();
+            // 분수 y / z 계산
+            long long yz_up = y.first * z.second;
+            long long yz_down = y.second * z.first;
+            long long gcd_yz = gcd(yz_up, yz_down);
+            yz_up /= gcd_yz;
+            yz_down /= gcd_yz;
 
-            s.push(sv(x, y, z));
-        } else {
-            if (i == "(") {
-                s.push(make_pair(-1, -1)); // '('를 표시하기 위한 더미 값
-            } else {
-                s.push(make_pair(stoll(i), 1));
-            }
+            // 분수 x + (y / z) 계산
+            long long result_up = x.first * yz_down + x.second * yz_up;
+            long long result_down = x.second * yz_down;
+            long long gcd_result = gcd(result_up, result_down);
+            result_up /= gcd_result;
+            result_down /= gcd_result;
+
+            s.push(make_pair(result_up, result_down));
+        }
+        else {
+            s.push(make_pair(stoll(token), 1)); // 숫자를 분수 형태로 변환
         }
     }
 
     if (s.size() != 1) {
         cout << -1 << endl;
-    } else {
+    }
+    else {
         cout << s.top().first << " " << s.top().second << endl;
     }
 
