cde00d3ee6983a5c63dfd6f9a6aab4f1c14ae614
고경우 <dfdfg11@gmail.com>
2024-04-13 08:40:38 +0900

    [Gold IV] Title: 알고스팟, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.cc
+++ 백준/Gold/1261. 알고스팟/알고스팟.cc
@@ -11,19 +11,17 @@ int dy[4] = { 0, 0, 1, -1 };
 
 vector<vector<int>> maze;
 vector<vector<int>> cost;
-vector<vector<bool>> visited;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    int m, n;
+    int n, m;
     cin >> m >> n;
 
     maze.resize(n, vector<int>(m, 0));
     cost.resize(n, vector<int>(m, INF));
-    visited.resize(n, vector<bool>(m, false));
 
     for (int i = 0; i < n; i++) {
         string s;
@@ -34,9 +32,8 @@ int main() {
     }
 
     priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
-    pq.push({ 0, {0, 0} });
+    pq.push({0, {0, 0}});
     cost[0][0] = 0;
-    visited[0][0] = true;
 
     while (!pq.empty()) {
         int currentCost = pq.top().first;
@@ -52,12 +49,12 @@ int main() {
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
+
             if (0 <= nx && nx < n && 0 <= ny && ny < m) {
                 int nextCost = currentCost + (maze[nx][ny] == 1 ? 1 : 0);
-                if (!visited[nx][ny] || nextCost < cost[nx][ny]) {
-                    visited[nx][ny] = true;
+                if (nextCost < cost[nx][ny]) {
                     cost[nx][ny] = nextCost;
-                    pq.push({ nextCost, {nx, ny} });
+                    pq.push({nextCost, {nx, ny}});
                 }
             }
         }
