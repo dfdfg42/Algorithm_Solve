0fddba8127e4e209bdb905277b55add4aecf28b0
고경우 <dfdfg11@gmail.com>
2024-05-06 22:23:39 +0900

    [Gold IV] Title: N-Queen, Time: 4304 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -3,12 +3,12 @@
 using namespace std;
 
 int N, answer = 0;
-vector<int> row; // row[i]는 i번째 행의 퀸의 위치 (열 번호)
+vector<int> row; 
 
-// 현재 위치에 퀸을 놓을 수 있는지 체크
+
 bool isPromising(int currentRow) {
     for (int i = 0; i < currentRow; ++i) {
-        // 같은 열 또는 대각선 상에 퀸이 있는지 확인
+       
         if (row[i] == row[currentRow] || abs(row[currentRow] - row[i]) == abs(currentRow - i)) {
             return false;
         }
@@ -16,14 +16,14 @@ bool isPromising(int currentRow) {
     return true;
 }
 
-// 백트래킹을 이용하여 모든 경우의 수 탐색
+
 void solve(int currentRow) {
-    if (currentRow == N) { // 모든 행에 퀸을 놓았다면
-        ++answer; // 경우의 수 증가
+    if (currentRow == N) { 
+        ++answer;
     } else {
         for (int i = 0; i < N; ++i) {
-            row[currentRow] = i; // 현재 행에 퀸을 놓음
-            if (isPromising(currentRow)) { // 유효한 위치라면 다음 행으로
+            row[currentRow] = i; 
+            if (isPromising(currentRow)) { 
                 solve(currentRow + 1);
             }
         }
@@ -33,7 +33,7 @@ void solve(int currentRow) {
 int main() {
     cin >> N;
     row.resize(N);
-    solve(0); // 0번 행부터 시작
+    solve(0); 
     cout << answer;
     return 0;
 }
