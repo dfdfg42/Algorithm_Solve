4b9bb52ec499df6fbdeb65ba0281872d1dcd6a14
고경우 <dfdfg11@gmail.com>
2024-07-01 13:22:30 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm> // for reverse()
+
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << 1 << '\n' << 0;
+
+    return 0;
+}
