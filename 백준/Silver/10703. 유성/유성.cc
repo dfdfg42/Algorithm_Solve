3cc05639a80fd509845e6897d3b412fc70ccebcf
고경우 <dfdfg11@gmail.com>
2024-07-11 19:07:43 +0900

    [Silver I] Title: 유성, Time: 584 ms, Memory: 10868 KB -BaekjoonHub

--- 백준/Silver/10703. 유성/유성.cc
+++ 백준/Silver/10703. 유성/유성.cc
@@ -1,18 +1,27 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstring>
+#include <cmath>
 #include <climits>
 
 using namespace std;
 
 int main() {
+    
     int r, s;
     cin >> r >> s;
 
-    vector<vector<char>> board(r, vector<char>(s));
+    //만약 가장 낮은 위치에 있는 유성과
+    //그 열에 가장 낮은 위치의 유성 - 가장 높은 위치에 있는 땅
+    //그 값들의 최소값 만큼 내려 올 수 있다.
+    
+    vector<vector<char>> board(r,vector<char>(s));
     for (int i = 0; i < r; i++) {
         string input;
         cin >> input;
+
+        //board 에 입력
         for (int j = 0; j < s; j++) {
             board[i][j] = input[j];
         }
@@ -20,44 +29,41 @@ int main() {
 
     int minDiff = INT_MAX;
 
-    // 각 열마다 유성의 최하단과 땅의 최상단을 찾아 최소 차이를 구함
+    //열을 순회하면서 가장 작은 차이를 고른다
     for (int i = 0; i < s; i++) {
         int maxMeteor = -1;
-        int minGround = INT_MAX;
-
+        int maxGround = INT_MAX;
         for (int j = 0; j < r; j++) {
             if (board[j][i] == 'X') {
                 maxMeteor = j;
             }
-            if (board[j][i] == '#' && minGround == INT_MAX) {
-                minGround = j;
+            if (board[j][i] == '#' && maxGround == INT_MAX) {
+                maxGround = j;
             }
         }
-        
         // 유성이 있고, 그 아래에 땅이 있는 경우만 계산
-        if (maxMeteor != -1 && minGround != INT_MAX) {
-            minDiff = min(minGround - maxMeteor - 1, minDiff);
+        if (maxMeteor != -1 && maxGround != INT_MAX) {
+            minDiff = min(maxGround - maxMeteor-1 , minDiff);
         }
     }
 
-    // minDiff 만큼 유성을 아래로 내림
-    vector<vector<char>> newBoard(r, vector<char>(s, '.'));
 
+    //minDiff 만큼 유성을 아래로 내린다.
+    //아래부터 탐색해서 중복을 없앤다
     for (int i = r - 1; i >= 0; i--) {
         for (int j = 0; j < s; j++) {
             if (board[i][j] == 'X') {
-                newBoard[i + minDiff][j] = 'X';
-            }
-            if (board[i][j] == '#') {
-                newBoard[i][j] = '#';
+                board[i][j] = '.';
+                board[i + minDiff][j] = 'X';
             }
         }
     }
 
-    // 결과 출력
+
+    //복원 후 출력
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < s; j++) {
-            cout << newBoard[i][j];
+            cout << board[i][j];
         }
         cout << '\n';
     }
