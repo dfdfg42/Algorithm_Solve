29d66925b71a1bf03109947b3422d583f4a2a7e6
고경우 <dfdfg11@gmail.com>
2024-07-18 08:25:54 +0900

    [Gold III] Title: 치즈, Time: 16 ms, Memory: 2372 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.cc
+++ 백준/Gold/2638. 치즈/치즈.cc
@@ -2,30 +2,34 @@
 #include <vector>
 using namespace std;
 
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
 int cheeseCount;
 int n, m;
 vector<vector<int>> world;
 vector<vector<bool>> check;
 
 bool boundary(int x, int y) {
-    return (0 <= x && x < n && 0 <= y && y < m);
+    if (0 <= x && x < n && 0 <= y && y < m) return true;
+
+    return false;
 }
 
 void findOx(int x, int y) {
+
     for (int i = 0; i < 4; i++) {
         int nx = x + dx[i];
         int ny = y + dy[i];
         if (boundary(nx, ny)) {
-            if (world[nx][ny] == 0 && !check[nx][ny]) {
+            if (world[nx][ny] == 0 && check[nx][ny] == false) {
                 check[nx][ny] = true;
                 findOx(nx, ny);
             }
         }
-    }
-}
 
+    }
+
+}
 void cheeseCheck() {
     vector<pair<int, int>> toMelt;
 
@@ -53,9 +57,21 @@ void cheeseCheck() {
     }
 }
 
+
+
+
+
+
 int main() {
+    //1. 일단 공기를 돌면서 치즈에 갖혀있는지 아닌지 판별한다
+    // ->가장자리 공기를 탐색하고 외부 탐색이 안 되었다면 내부 공기
+    //2.치즈를 녹이고 시간을 보낸후 치즈가 0개라면 시간 출력
+
+
+
     cin >> n >> m;
-    world.resize(n, vector<int>(m, 0));
+    world.resize(n, vector<int>(m,0));
+ 
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
@@ -67,11 +83,12 @@ int main() {
     int phase = 0;
     while (cheeseCount > 0) {
         check.assign(n, vector<bool>(m, false));
-        // 외부 공기 확인
+        //외부 공기 확인
         check[0][0] = true;
         findOx(0, 0);
-        // 접촉 치즈 확인
+        //접촉 치즈 확인
         cheeseCheck();
+        
         phase++;
     }
 
@@ -79,3 +96,5 @@ int main() {
 
     return 0;
 }
+
+
