877725b28e795e0c7cced0902a8205f224240ae9
고경우 <dfdfg11@gmail.com>
2024-07-21 10:57:57 +0900

    [Silver IV] Title: 치즈버거 만들기 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/30020. 치즈버거 만들기 2/치즈버거 만들기 2.cc
+++ 백준/Silver/30020. 치즈버거 만들기 2/치즈버거 만들기 2.cc
@@ -1,51 +1,48 @@
-#include <bits/stdc++.h>
-#define ll long long
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include<queue>
+#include<unordered_map>
+#include <unordered_set>
 using namespace std;
-
-// Default
-
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
-// PBDS
-
+int x, y, buger, set;
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    ll A, B;
-    cin >> A >> B;
-
-    // 조건을 만족하지 않는 경우
-    if (A <= B || A > 2 * B) {
+    cin >> x >> y;
+    set = y; // 패티 치즈 쌍
+    buger = (x - y); //최소 버거갯수
+    if (y >= x) {
         cout << "NO";
-        return 0;
     }
-
-    cout << "YES\n";
-    vector<string> ans;
-
-    // A와 B의 차이가 1이 될 때까지 'aba'를 추가
-    while (B + 1 != A) {
-        ans.push_back("aba");
-        A -= 2;
-        B -= 1;
+    else if (buger > set)
+    {
+        cout << "NO";
     }
+    else {
+        cout << "YES" << "\n";
+        cout << buger << "\n";
+        for (int i = 0; i < buger; i++)
+        {
+            if (i == buger - 1) {
+                for (int i = 0; i < set; i++)
+                {
+                    cout << "ab";
+                }
+                cout << "a" << "\n";
+            }
+            else
+            {
+                cout << "aba" << "\n";
+                set--;
+            }
+        }
 
-    // 정답의 개수 출력
-    cout << ans.size() + 1 << "\n";
 
-    // 저장된 'aba' 문자열 출력
-    for (string s : ans) {
-        cout << s << "\n";
     }
-
-    // 남은 패티와 치즈로 'ab'를 반복 출력하고 마지막에 'a' 추가
-    for (int i = 0; i < B; ++i) {
-        cout << "ab";
-    }
-    cout << "a" << "\n";
 
     return 0;
 }
