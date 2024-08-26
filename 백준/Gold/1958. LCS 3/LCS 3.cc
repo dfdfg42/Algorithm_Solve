bb4c1a22187e4bf2fee7590870795bbc036bb2ed
고경우 <dfdfg11@gmail.com>
2024-08-26 20:49:04 +0900

    [Gold IV] Title: LCS 3, Time: 4 ms, Memory: 6048 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.cc
+++ 백준/Gold/1958. LCS 3/LCS 3.cc
@@ -1,35 +1,29 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
 
-const int MN = 101;
+const int nm = 101;
+int dp[nm][nm][nm];
 
-int dp[MN][MN][MN];
-
-int max(int a, int b, int c) { //인자 3개짜리 최대값 함수
+int max(int a, int b, int c) {
     return max(max(a, b), c);
 }
 
 int main() {
     string s1, s2, s3;
-    cin >> s1 >> s2 >> s3; //3개의 문자열 입력
-    s1 = ' ' + s1;
-    s2 = ' ' + s2;
-    s3 = ' ' + s3;
-    for(int i = 1; i < s1.size(); i++) {
-        for(int j = 1; j < s2.size(); j++) {
-            for(int k = 1; k < s3.size(); k++) {
-                if(s1[i] == s2[j] && s2[j] == s3[k]) { //3개 문자가 모두 같은 경우
-                    dp[i][j][k] = dp[i-1][j-1][k-1] + 1; //이전 공통 부분 문자열 크기에 1을 더함
-                }
-                else { //같지 않은 경우
-                    dp[i][j][k] = max(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1]); //지금까지의 최대값 저장
+    cin >> s1 >> s2 >> s3; // 3개의 문자열 입력
+
+    for (int i = 1; i <= s1.size(); i++) {
+        for (int j = 1; j <= s2.size(); j++) {
+            for (int k = 1; k <= s3.size(); k++) {
+                if (s1[i-1] == s2[j-1] && s2[j-1] == s3[k-1]) { // 3개 문자가 모두 같은 경우
+                    dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
+                } else { // 같지 않은 경우
+                    dp[i][j][k] = max(dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1]);
                 }
             }
         }
     }
-    cout << dp[s1.size()-1][s2.size()-1][s3.size()-1];
+    cout << dp[s1.size()][s2.size()][s3.size()];
+
+    return 0;
 }
