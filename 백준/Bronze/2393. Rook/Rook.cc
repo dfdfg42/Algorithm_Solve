9ce8b7880c8598d73204ee7d523abc0135ed5196
고경우 <dfdfg11@gmail.com>
2024-09-10 22:35:36 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 첫 번째 줄
+    cout << "  ___  ___  ___" << endl;
+    // 두 번째 줄
+    cout << "  | |__| |__| |" << endl;
+    // 세 번째 줄
+    cout << "  |           |" << endl;
+    // 네 번째 줄
+    cout << "   \\_________/" << endl;
+    // 다섯 번째 줄
+    cout << "    \\_______/" << endl;
+    // 여섯 번째 줄
+    cout << "     |     |" << endl;
+    // 일곱 번째 줄
+    cout << "     |     |" << endl;
+    // 여덟 번째 줄
+    cout << "     |     |" << endl;
+    // 아홉 번째 줄
+    cout << "     |     |" << endl;
+    // 열 번째 줄
+    cout << "     |_____|" << endl;
+    // 열한 번째 줄
+    cout << "  __/       \\__" << endl;
+    // 열두 번째 줄
+    cout << " /             \\" << endl;
+    // 열세 번째 줄
+    cout << "/_______________\\" << endl;
+
+    return 0;
+}
