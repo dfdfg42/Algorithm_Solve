11193083276148812e1b51d7926213fda8844b98
고경우 <dfdfg11@gmail.com>
2024-09-27 23:47:40 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        ll a, b;
+        cin >> a >> b;
+
+        cout << a + b<<'\n';
+
+    }
+   
+    return 0;
+}
