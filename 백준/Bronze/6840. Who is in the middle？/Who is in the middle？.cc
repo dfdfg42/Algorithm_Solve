1f941fb55428c91cb4eeb9d7fa1cc79b9bc9976b
고경우 <dfdfg11@gmail.com>
2025-06-27 22:41:09 +0900

    [Bronze V] Title: Who is in the middle?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+    vector<int> v = {a, b, c};
+    sort(v.begin(), v.end());
+
+    cout << v[1] << "\n";
+
+    return 0;
+}
