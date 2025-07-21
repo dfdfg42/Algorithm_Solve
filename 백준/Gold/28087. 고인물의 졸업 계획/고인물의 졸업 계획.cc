73a5ace95571f52d67a954262d5ea850917ff5b1
고경우 <dfdfg11@gmail.com>
2025-07-21 18:11:17 +0900

    [Gold IV] Title: 고인물의 졸업 계획, Time: 1040 ms, Memory: 124328 KB -BaekjoonHub

---
+++ 백준/Gold/28087. 고인물의 졸업 계획/고인물의 졸업 계획.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <tuple>
+#include <algorithm>
+#include <map>
+#include <deque>
+
+
+using namespace std;
+
+typedef long long ll;
+
+int n, m;
+vector<pair<int,int>> subjs; // 값,순서
+deque<pair<ll, int>> ans;
+
+void solve() {
+   cin >> n >> m;
+   subjs.reserve(m + 1);
+   for (int i = 1; i <= m; i++) {
+       int input;
+       cin >> input;
+       subjs.push_back({ input,i });
+   }
+
+   sort(subjs.begin(), subjs.end()); 
+
+   ll sum = 0;
+   for (int i = 0; i < m; ++i) {
+       ans.push_back({ subjs[i].first, subjs[i].second });
+       sum += subjs[i].first;
+
+       while (!ans.empty() && sum > 2LL * n) {
+           sum -= ans.front().first;
+           ans.pop_front();
+       }
+
+       if (sum >= n && sum <= 2LL * n)
+           break;
+   }
+
+   cout << ans.size() << '\n';
+   for (auto a : ans) {
+       cout << a.second << '\n';
+   }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    
+
+    return 0;
+}
