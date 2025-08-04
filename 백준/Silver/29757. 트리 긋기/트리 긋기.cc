b1c316a674662cdb1a624c0ced241151ca7a8b74
고경우 <dfdfg11@gmail.com>
2025-08-04 10:27:07 +0900

    [Silver II] Title: 트리 긋기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/29757. 트리 긋기/트리 긋기.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <tuple>
+#include <algorithm>
+#include <map>
+#include <deque>
+#include <string>
+#include <sstream>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+    
+    int n;
+    cin >> n;
+
+    vector<pair<pair<int, int>, int>> points;
+
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        points.push_back({{y,x},i + 1 });
+    }
+
+    sort(points.begin(), points.end());
+
+    for (int i = 1; i < n; i++) {
+        cout <<  points[i-1].second <<' ' << 
+        points[i].second <<'\n';
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
