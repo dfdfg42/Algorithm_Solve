4c531be68041eb2b780dcc08f0fce7d9574aada2
고경우 <dfdfg11@gmail.com>
2025-09-08 23:40:42 +0900

    [Silver I] Title: 더 게임 오브 데스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/32248. 더 게임 오브 데스/더 게임 오브 데스.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+ll n, t;
+vector<int> a;
+vector<bool> visited;
+
+//사이클 크기만 검출
+int dfs(int now , int start ,int cnt) {
+
+    if (now == start) {
+        return cnt;
+    }
+    else {
+        cnt++;
+        return dfs(a[now], start, cnt);
+    }
+
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    cin >> n >> t;
+    a.assign(n + 1,0);
+    visited.assign(n + 1, false);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    int now = 1;
+    bool cycled = false;
+    while (t > 0) {
+        visited[now] = true;
+        now = a[now];
+        t--;
+        if (visited[now] && !cycled) {
+            int cycleCount = dfs(a[now], now, 1);
+
+            if (cycleCount == 0) {
+                cycleCount = 1;
+            }
+            t %= cycleCount;
+            cycled = true;
+        }
+
+    }
+
+    cout << now << '\n';
+
+    return 0;
+}
