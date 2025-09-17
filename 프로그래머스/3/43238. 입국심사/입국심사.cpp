795914b6193980b7737d21556f587eb7911dff10
고경우 <dfdfg11@gmail.com>
2025-09-17 21:16:10 +0900

    [level 3] Title: 입국심사, Time: 43.38 ms, Memory: 6.76 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+
+long long solution(int n, vector<int> times) {
+    
+     ll ans = 0;
+    sort(times.begin(), times.end());
+    
+    
+    ll l = 0;
+    ll r =  (long long)times.back() * n;
+    while(l<=r){
+        //mid = 흐른 시간
+        ll mid = (l+r)/2;
+        
+        ll cnt = 0;
+        for(auto a : times){
+            cnt += mid/a;
+            if (cnt >= n) break;
+        }
+        if(cnt >= n){
+            ans = mid;
+            r = mid-1;
+        }
+        else if(cnt < n){
+            l = mid+1;
+        }
+    }
+    
+    
+    
+    return ans;
+}
