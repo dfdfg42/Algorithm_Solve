9fede7c5079c6f916e2123ead46065eaf31c3aeb
고경우 <dfdfg11@gmail.com>
2025-10-08 19:05:45 +0900

    [Silver II] Title: 인터뷰, Time: 24 ms, Memory: 3588 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/27914. 인터뷰/인터뷰.cc
+++ 백준/Silver/27914. 인터뷰/인터뷰.cc
@@ -13,46 +13,37 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    //연속된길이! = 가짓수
 
     int n, k, q;
     cin >> n >> k >> q;
 
-    vector<ll> dp(4001,0);
+    vector<ll> dp(n,0);
 
-    dp[1] = 1;
-    for (int i = 2; i <= 4000; i++) {
-        dp[i] = dp[i - 1] + i;
-        //cout << dp[i] << '\n';
-    }
 
     vector<int> v(n);
     vector<int> Q(q);
     for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
-    for (int i = 0; i < q; i++) {
-        cin >> Q[i];
+
+    int seq = 0;
+    for (int i = 0; i < n; i++) {
+  
+
+        if (v[i] != k) {
+            seq++;
+        }
+        else {
+            seq = 0;
+        }
+        if (i == 0) dp[0] = dp[0] + seq;
+        else
+        dp[i] = dp[i - 1] + seq;
     }
 
-    for (auto a : Q) {
-
-        int seq = 0;
-        ll ans = 0;
-        for (int i = 0; i < a; i++) {
-            
-            if (v[i] != k) {
-                seq++;
-                
-            }
-            else {
-                ans += dp[seq];
-                seq = 0;
-            }
-        }
-        ans += dp[seq];
-
-        cout << ans << '\n';
+    while(q--) {
+        int input; cin >> input;
+        cout << dp[input - 1] << '\n';
     }
 
     return 0;
