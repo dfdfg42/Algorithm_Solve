ba1e9206df0047da8de06f19e26cca152dae54d0
고경우 <dfdfg11@gmail.com>
2025-11-21 14:24:05 +0900

    [Silver I] Title: 소수 사이 수열, Time: 12 ms, Memory: 3076 KB -BaekjoonHub

---
+++ 백준/Silver/3896. 소수 사이 수열/소수 사이 수열.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cmath>
+#include <tuple>
+#include <vector>
+#include <map>
+#include <queue> 
+#include <algorithm>
+
+using namespace std;
+
+vector<int> primes;
+
+void solve() {
+	int k;
+	cin >> k;
+
+	if (binary_search(primes.begin(), primes.end(), k)) {
+		cout << 0 << '\n';
+		return;
+	}
+
+	int l = 0, r = primes.size() - 1;
+	int ansIdx = 0; 
+
+	while (l <= r) {
+		int mid = (l + r) / 2;
+		if (primes[mid] > k) {
+			r = mid - 1;
+		}
+		else {
+			ansIdx = mid;   
+			l = mid + 1;
+		}
+	}
+
+	cout << primes[ansIdx + 1] - primes[ansIdx] << '\n';
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	vector<bool> check(1299710, false);
+
+	for (int i = 2; i <= 1299709; i++) {
+		
+		if (!check[i]) {
+			primes.push_back(i);
+			for (int j = i; j <= 1299709; j += i) {
+				check[j] = true;
+			}
+		}
+
+	}
+
+	//for (auto a : primes) {
+	//	cout << a << '\n';
+	//}
+
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		solve();
+	}
+
+	return 0;
+}
