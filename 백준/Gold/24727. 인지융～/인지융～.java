54eac22da5cefe2c6b121dc9f25a1ef446a55cec
고경우 <dfdfg11@gmail.com>
2026-02-05 14:48:12 +0900

    [Gold III] Title: 인지융~, Time: 64 ms, Memory: 11676 KB, Score: 96 point -BaekjoonHub

--- 백준/Gold/24727. 인지융～/인지융～.java
+++ 백준/Gold/24727. 인지융～/인지융～.java
@@ -2,125 +2,80 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
-    static int N;
-    static int A, B;
-    static int board[][];
-    static int dyA[] = {0,0,1};
-    static int dxA[] = {1,-1,0};
-    
-    static int dyB[] = {0,0,-1};
-    static int dxB[] = {-1,1,0};
-    
-    static int dy[] = {-1,0,1,0};
-    static int dx[] = {0,1,0,-1};
-     
-    static int lastBy;
-    static int lastBx;
-    
-    static boolean check(int y, int x) {
-    	if(y<0 || y>=N || x<0 || x>=N) return false;
-    	
-    	return true;
-    }
-    
-    static void dfsA(int y, int x ) {
-		board[y][x] = 2;
-		A--;
-    	if(A == 0 ) return;
-    		
-    	for(int i=0;i<3; i++) {
-    		int ny = y + dyA[i];
-    		int nx = x + dxA[i];
-    		
-    		if(!check(ny,nx)) continue;
-    		
-    		if( board[ny][nx] != 0) continue;
-    		
+    static int N, C, E;
+    static int[][] board;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        C = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
 
-    		dfsA(ny,nx);
-    		
-    		break;
-    	}
-    	
-    	return;
-    }
-    
-    static void dfsB(int y, int x ) {
-    	board[y][x] = 1;
-    	lastBy = y;
-    	lastBx = x;
-    	
-		B--;
-    	if(B == 0 ) return;
-		
-    	for(int i=0;i<3; i++) {
-    		int ny = y + dyB[i];
-    		int nx = x + dxB[i];
-    		
-    		if(!check(ny,nx)) continue;
-    		
-    		if( board[ny][nx] != 0) continue;
-    		
-    		dfsB(ny,nx);
-    		
-    		break;
-    	}
-    	
-    	return;
-    }
-    
-    static boolean can() {
-    	
-    	for(int i=0; i<4; i++) {
-    		int ny = lastBy +dy[i];
-    		int nx = lastBx + dx[i];
-    		
-    		if(!check(ny,nx)) continue;
-    		
-    		if(board[ny][nx] == 2) return false;
-    	}
-    	
-    	return true;
-    }
-    
-   
-    public static void main(String args[]) throws IOException {
-       	StringTokenizer st = new StringTokenizer(br.readLine());
-       	StringBuilder sb = new StringBuilder();
-       	
-       	N = Integer.parseInt(st.nextToken());
-       	
-       	board = new int[N][N];
-       	
-       	st = new StringTokenizer(br.readLine());
-       	A = Integer.parseInt(st.nextToken());
-       	B = Integer.parseInt(st.nextToken());
-       	
-       	if(N%2 ==0) {
-           	dfsA(0,0);
-       	}else dfsA(0,N-1);
+        board = new int[N][N];
 
-       	dfsB(N-1,N-1);
-       	
-       	//System.out.println(lastBy + " " + lastBx);
-       	
-       	if(!can()) {
-       		sb.append(-1 + "\n");
-       	}
-       	else {
-       		sb.append(1 + "\n");
-       		for(int i=0; i<N; i++) {
-       			for(int j=0; j<N; j++) {
-       				sb.append(board[i][j]);
-       			}
-       			sb.append("\n");
-       		}
-       	}
-       	
-       	System.out.println(sb.toString());
+        // 1. 영역 1 채우기 (왼쪽 위부터 대각선 방향으로)
+        int count1 = 0;
+        outer1:
+        for (int sum = 0; sum <= 2 * (N - 1); sum++) {
+            for (int i = 0; i < N; i++) {
+                int j = sum - i;
+                if (j >= 0 && j < N) {
+                    board[i][j] = 1;
+                    count1++;
+                    if (count1 == C) break outer1;
+                }
+            }
+        }
 
+        // 2. 영역 2 채우기 (오른쪽 아래부터 대각선 방향으로)
+        int count2 = 0;
+        outer2:
+        for (int sum = 2 * (N - 1); sum >= 0; sum--) {
+            for (int i = N - 1; i >= 0; i--) {
+                int j = sum - i;
+                if (j >= 0 && j < N) {
+                    if (board[i][j] == 1) continue; // 이미 1이 차있으면 건너뜀
+                    board[i][j] = 2;
+                    count2++;
+                    if (count2 == E) break outer2;
+                }
+            }
+        }
+
+        // 3. 인접 여부 검사
+        if (isAdjacent()) {
+            System.out.println("-1");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            sb.append("1\n");
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    sb.append(board[i][j]);
+                }
+                sb.append("\n");
+            }
+            System.out.print(sb);
+        }
+    }
+
+    static boolean isAdjacent() {
+        int[] dy = {-1, 1, 0, 0};
+        int[] dx = {0, 0, -1, 1};
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 1) {
+                    for (int d = 0; d < 4; d++) {
+                        int ni = i + dy[d];
+                        int nj = j + dx[d];
+                        if (ni >= 0 && ni < N && nj >= 0 && nj < N) {
+                            if (board[ni][nj] == 2) return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
