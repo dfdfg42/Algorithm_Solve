70f162db6a5d65ccb76826cb6cc9353d50a80047
고경우 <dfdfg11@gmail.com>
2026-02-07 22:57:24 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << "LoveisKoreaUniversity ";
+    }
+
+    return 0;
+}
