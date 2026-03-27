1ff5c1d12c55440e7e7414745409bc7e90d7502a
고경우 <dfdfg11@gmail.com>
2026-03-27 13:57:37 +0900

    [Gold IV] Title: 로또, Time: 0 ms, Memory: 2296 KB -BaekjoonHub

---
+++ 백준/Gold/2758. 로또/로또.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+
+	vector<vector<long long>> dp(11, vector<long long>(2001, 0));
+
+	for (int j = 0; j <= 2000; j++) {
+		dp[0][j] = 1;
+	}
+
+	for (int i = 1; i <= 10; i++) {
+		for (int j = 1; j <= 2000; j++) {
+			dp[i][j] = dp[i][j - 1] + dp[i - 1][j / 2];
+		}
+	}
+
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+		cout << dp[a][b] << '\n';
+	}
+
+	return 0;
+}
