ef6e997ad800e1660359a8f140d3c26b0034191b
고경우 <dfdfg11@gmail.com>
2026-04-11 23:25:26 +0900

    [Bronze V] Title: NFC West vs North, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 줄바꿈과 띄어쓰기를 예제와 동일하게 출력합니다.
+    cout << "NFC West       W   L  T" << endl;
+    cout << "-----------------------" << endl;
+    cout << "Seattle        13  3  0" << endl;
+    cout << "San Francisco  12  4  0" << endl;
+    cout << "Arizona        10  6  0" << endl;
+    cout << "St. Louis      7   9  0" << endl;
+    cout << "" << endl; // 중간 빈 줄
+    cout << "NFC North      W   L  T" << endl;
+    cout << "-----------------------" << endl;
+    cout << "Green Bay      8   7  1" << endl;
+    cout << "Chicago        8   8  0" << endl;
+    cout << "Detroit        7   9  0" << endl;
+    cout << "Minnesota      5  10  1" << endl;
+
+    return 0;
+}
