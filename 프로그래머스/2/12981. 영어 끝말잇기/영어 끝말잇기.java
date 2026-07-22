dd8e8f89f545af4981c37c6718aa403f0ed9ae3c
고경우 <dfdfg11@gmail.com>
2026-07-22 12:47:48 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        
+        HashSet<String> s = new HashSet<>();
+        int wlength = words.length;
+        String prevWord = words[0];
+        s.add(words[0]);
+        
+        for(int i=1; i<wlength; i++){
+            
+            int people = i % n;
+            
+            if(s.contains(words[i]) || words[i].charAt(0) != prevWord.charAt(prevWord.length()-1)){
+                System.out.println(i);
+                return new int[] {(i+1)-(i/n)*n ,(i+n)/n};
+            }else{
+                s.add(words[i]);
+                prevWord = words[i];
+            }
+            
+            
+        }
+
+        return new int[]{0,0};
+    }
+}
